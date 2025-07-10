05cccc9cab27680a90dc7b9a493bfe9878ec4baa
Silas-Ce <ceazersilas@gmail.com>
2025-07-10 17:51:01 +0200

    Update Goal.kt

--- app/src/main/java/com/yodiet/data/db/model/Goal.kt
+++ app/src/main/java/com/yodiet/data/db/model/Goal.kt
@@ -1,2 +1,14 @@
 package com.yodiet.data.db.model
 
+data class Goal(
+    val id: Int,
+    var name: String,
+    var type: String,
+    var description: String,
+    var currentValue: Float,
+    var targetValue: Float,
+) {
+    val progress: Float
+        get() = if (targetValue == 0f) 0f else (currentValue / targetValue).coerceIn(0f, 1f)
+}
+
